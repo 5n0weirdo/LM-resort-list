dcef90e1121d98cee2730586c08b0ad0ef35163d
5n0weirdo <snoweirdo@protonmail.com>
2021-10-17 17:32:51 +0200

    Adding data to list

--- src/data.js
+++ src/data.js
@@ -2,16 +2,24 @@ export default [
   {
     id: 1,
     name: 'Four Seasons Resort Lanai',
-    rating: "5.0 stars",
+    rating: "5.0 star",
     image: "https://travel.usnews.com/dims4/USNEWS/b5ae635/2147483647/resize/255x255%5E%3E/crop/255x255/quality/85/?url=https://travel.usnews.com/images/Four_Seasons_Resort_Lanai_at_Manele_Bay_usn_8.jpg"
 
   },
   {
     id: 2,
-    name: "blah",
-    rating: "6",
+    name: "Belmond Cap Juluca",
+    rating: "5.0 star",
     image: "https://travel.usnews.com/dims4/USNEWS/214eb1b/2147483647/resize/255x255%5E%3E/crop/255x255/quality/85/?url=https://travel.usnews.com/images/Belmond_Cap_Juluca_usn_3.jpg"
+  },
+
+  {
+    id: 3,
+    name: "The St. Regis Bahia Beach Resort, Puerto",
+    rating: "5.0 star",
+    image: "https://travel.usnews.com/dims4/USNEWS/dee631c/2147483647/resize/255x255%5E%3E/crop/255x255/quality/85/?url=https://travel.usnews.com/images/000_fIEdJFU.jpg"
   }
+
 
 
 ]
