ea3ff1ca481146c1ed3c89c14261644bcfc38d4c
5n0weirdo <snoweirdo@protonmail.com>
2021-10-18 06:49:22 +0200

    Styled container

--- src/data.js
+++ src/data.js
@@ -1,25 +1,39 @@
 export default [
   {
     id: 1,
-    name: 'Four Seasons Resort Lanai',
+    name: "Four Seasons Resort Lanai",
     rating: "5.0 star",
-    image: "https://travel.usnews.com/dims4/USNEWS/b5ae635/2147483647/resize/255x255%5E%3E/crop/255x255/quality/85/?url=https://travel.usnews.com/images/Four_Seasons_Resort_Lanai_at_Manele_Bay_usn_8.jpg"
-
+    image:
+      "https://travel.usnews.com/dims4/USNEWS/b5ae635/2147483647/resize/255x255%5E%3E/crop/255x255/quality/85/?url=https://travel.usnews.com/images/Four_Seasons_Resort_Lanai_at_Manele_Bay_usn_8.jpg",
   },
   {
     id: 2,
     name: "Belmond Cap Juluca",
     rating: "5.0 star",
-    image: "https://travel.usnews.com/dims4/USNEWS/214eb1b/2147483647/resize/255x255%5E%3E/crop/255x255/quality/85/?url=https://travel.usnews.com/images/Belmond_Cap_Juluca_usn_3.jpg"
+    image:
+      "https://travel.usnews.com/dims4/USNEWS/214eb1b/2147483647/resize/255x255%5E%3E/crop/255x255/quality/85/?url=https://travel.usnews.com/images/Belmond_Cap_Juluca_usn_3.jpg",
   },
 
   {
     id: 3,
     name: "The St. Regis Bahia Beach Resort, Puerto",
     rating: "5.0 star",
-    image: "https://travel.usnews.com/dims4/USNEWS/dee631c/2147483647/resize/255x255%5E%3E/crop/255x255/quality/85/?url=https://travel.usnews.com/images/000_fIEdJFU.jpg"
-  }
+    image:
+      "https://travel.usnews.com/dims4/USNEWS/dee631c/2147483647/resize/255x255%5E%3E/crop/255x255/quality/85/?url=https://travel.usnews.com/images/000_fIEdJFU.jpg",
+  },
 
-
-
-]
+  {
+    id: 4,
+    name: "Tortuga Bay",
+    rating: "5.0 star",
+    image:
+      "https://travel.usnews.com/dims4/USNEWS/c2fc127/2147483647/resize/255x255%5E%3E/crop/255x255/quality/85/?url=https://travel.usnews.com/images/Tortuga_Bay_usn_4.jpg",
+  },
+  // {
+  //   id: 5,
+  //   name: "The Inn & Club at Harbour Town - The Sea Pines Resort",
+  //   rating: "5-0 star",
+  //   image:
+  //     "https://travel.usnews.com/dims4/USNEWS/d077018/2147483647/resize/255x255%5E%3E/crop/255x255/quality/85/?url=https://travel.usnews.com/images/The_Inn_Club_at_Harbour_Town_-_Sea_Pines_Resort_usn_13.jpg",
+  // },
+];
